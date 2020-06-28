a3baf6f408b1b270f08bcfcf2a4f7abb305f7edf
ixi150 <ixi150@gmail.com>
2020-06-28 15:08:06 +0200

    added cursors

--- Assets/Code/PlayerInputManager.cs
+++ Assets/Code/PlayerInputManager.cs
@@ -1,9 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerInputManager : MonoBehaviour
 {
-    [SerializeField] BeltManager beltManager;
+    [SerializeField] BeltManager beltManager = null;
+    [SerializeField] Button dragButton = null;
+    [SerializeField] Button beltButton = null;
+
+    public static PlayerMode PlayerMode;
+
+    void Awake()
+    {
+        PlayerMode = PlayerMode.Dragging;
+
+        dragButton.onClick.AddListener(() => PlayerMode = PlayerMode.Dragging);
+        beltButton.onClick.AddListener(() => PlayerMode = PlayerMode.Belts);
+    }
 
     void Update()
     {
@@ -15,9 +28,28 @@ public class PlayerInputManager : MonoBehaviour
 #endif
         }
 
-        var ctrlPressed = Input.GetKey(KeyCode.LeftControl);
 
-        beltManager.enabled = ctrlPressed;
-        Draggable.IsDragEnabled = !ctrlPressed;
+
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            PlayerMode = PlayerMode.Belts;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl) && PlayerMode == PlayerMode.Belts)
+        {
+            PlayerMode = PlayerMode.Dragging;
+        }
+
+
+        dragButton.interactable = !(Draggable.IsDragEnabled = PlayerMode == PlayerMode.Dragging);
+        beltButton.interactable = !(beltManager.enabled = PlayerMode == PlayerMode.Belts);
     }
+}
+
+public enum PlayerMode
+{
+    None,
+    Dragging,
+    Belts,
+    Building,
 }
---
+++ Assets/Code/UI/CursorManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CursorManager : MonoBehaviour
+{
+    [SerializeField] CursorSetting normalCursor = default;
+    [SerializeField] CursorSetting dragCursor = default;
+    [SerializeField] CursorSetting draggingCursor = default;
+    [SerializeField] CursorSetting beltCursor = default;
+
+    void Update()
+    {
+        var setting = GetCursor();
+        Cursor.SetCursor(setting.texture, setting.hotspot, CursorMode.Auto);
+    }
+
+    CursorSetting GetCursor()
+    {
+        switch (PlayerInputManager.PlayerMode)
+        {
+            case PlayerMode.Dragging:
+                return Draggable.IsAnythingDragged ? draggingCursor : dragCursor;
+            case PlayerMode.Belts:
+                return beltCursor;
+            case PlayerMode.Building:
+                return default; //todo
+            default:
+                return normalCursor;
+        }
+    }
+
+    [Serializable]
+    struct CursorSetting
+    {
+        public Texture2D texture;
+        public Vector2 hotspot;
+    }
+}
